43562c19e7eabb8400ec86f07a52796a1b7de7b8
Trinity Xia <hnaoto@me.com>
2016-11-19 17:06:52 -0500

    Model: add team and defense model

--- dbdemo/models.py
+++ dbdemo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    defenseID = models.ForeignKey(Defense)
+
+class Defense(models.Model):
+    pointsAllowed = models.IntegerField(default=0)
+    sacks = models.IntegerField(default=0)
+    interceptions = models.IntegerField(default=0)
+    fumbleRecoveries = models.IntegerField(default=0)
+    defTD = models.IntegerField(default=0)
+    passRank = models.IntegerField(default=0)
+    rushRank = models.IntegerField(default=0)
+    puntRetRank = models.IntegerField(default=0)
+    kickRetRank = models.IntegerField(default=0)
+
