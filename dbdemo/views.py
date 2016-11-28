46e959fe2ad519480416f01cd14fcf149ead4c4d
bingle2400 <zq2sa@virginia.edu>
2016-11-28 13:26:34 -0500

    sql query work v1

--- dbdemo/views.py
+++ dbdemo/views.py
@@ -11,13 +11,31 @@ from .models import Injury
 from .models import Suspended
 from .models import CareerStats
 from .models import SeasonOffensiveStats
+from django.db import connection,transaction
 
-
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+        ]
 
 def index(request):
- querySet  = Defense.objects.all()
- context ={
-   "object_list": querySet
+    name = request.GET.get("n")
+    cursor = connection.cursor()
 
- }
- return render(request,'index.html', context)
+    cursor.execute('''SELECT * FROM dbdemo_Players WHERE name =%s''',[name])
+
+    playerSet  = dictfetchall(cursor)
+
+    cursor.execute('''SELECT * FROM dbdemo_Team WHERE name =%s''',[name])
+    teamSet = dictfetchall(cursor)
+
+    cursor.close()
+    context ={
+        "player_list": playerSet,
+        "team_list": teamSet,
+
+    }
+    return render(request,'index.html', context)
